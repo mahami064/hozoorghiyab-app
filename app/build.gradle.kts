9ad3917d5c0a9ac63ae325553bcf436fb6a865f1
mahami064 <55809870+mahami064@users.noreply.github.com>
2026-06-23 00:40:37 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,10 @@ android {
       isCrunchPngs = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
+     
     }
     debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
+   
     }
   }
   compileOptions {
